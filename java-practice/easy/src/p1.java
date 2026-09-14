526a0217f2649989da44c676794aa53de8cce120
Mun kyeonguk <ukumoon365@gmail.com>
2026-09-14 20:16:47 +0900

    Solve: Q1 양수 판정

---
+++ java-practice/easy/src/p1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // 양수 판정 if문
+        if (n > 0) {
+            System.out.println("양수입니다");
+        }
+    }
+}
